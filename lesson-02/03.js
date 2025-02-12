bf670687d30a0f9e96f24a5de4a4a049c0ab11f0
VmNjord <alivenjord@gmail.com>
2025-02-12 18:02:00 +0300

    test 2.3.1.

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,25 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 50 // тестовое значение, можно изменять
 let grade
 // your code
+if (score === 100 & score >= 90) {
+    grade = "A" 
+} else if
+(score <= 89 & score >= 80) {
+grade = "B"
+}
+else if
+(score <= 79 & score >= 70) {
+grade = "C"
+}
+else if
+(score <= 69 & score >= 50) {  
+grade = "D"
+}
+else if
+(score <= 49 & score >= 0) {
+grade = "F"
+}
+console.log(grade); // Выводим оценку

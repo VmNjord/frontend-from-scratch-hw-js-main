935f4f82aa2cf68ad7496ef9daa492910298a592
VmNjord <alivenjord@gmail.com>
2025-02-12 17:27:23 +0300

    test 2.2.2.

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,7 +11,7 @@ let dice1 = Math.floor(Math.random() * 7); // your code
 let dice2 = Math.floor(Math.random() * 7);// your code Генерируем случайное число от 1 до 6
 let isWinningDouble = dice1 === dice2;// your code  Сравниваем 1 и 2 бросок
 
-/*
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +19,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+

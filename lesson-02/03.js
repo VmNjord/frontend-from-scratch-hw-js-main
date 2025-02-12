ab689c0ab1087e02d5c7d2a91e24761047d7820b
VmNjord <alivenjord@gmail.com>
2025-02-12 18:35:25 +0300

    test 2.3.5

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,7 +4,7 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = Math.floor(Math.random() * 101); // тестовое значение, можно изменять
+const score = 56; // тестовое значение, можно изменять
 let grade;
 // your code
 if (score >= 0 && score < 50) {

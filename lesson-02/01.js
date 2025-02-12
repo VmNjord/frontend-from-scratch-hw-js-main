95bc9f86d2066b413f9f9bd223dd6c3b9503efeb
VmNjord <alivenjord@gmail.com>
2025-02-12 17:07:53 +0300

    test 2.1.1.

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,20 +8,22 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
+let isAccess;
 
 // your code
 
-isAccess =  // проверяем выполнение условий, для получения доступа
-(isAdmin === true || isVerifiedUser === true) && 
- // есть статус администратора или подтверждённый пользователь
-(hasSpecialPermission === true || hasTemporaryPass === true);
- // обладает специальным разрешение или временным пропуском
+// isAccess =  // проверяем выполнение условий, для получения доступа
+// (isAdmin === true || isVerifiedUser === true) && 
+//  // есть статус администратора или подтверждённый пользователь
+// (hasSpecialPermission === true || hasTemporaryPass === true);
+//  // обладает специальным разрешение или временным пропуском
+
+isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass);
 
 console.log(isAccess);
 

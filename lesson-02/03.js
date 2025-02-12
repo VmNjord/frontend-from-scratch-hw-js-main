f4ed858d66cd0bb01b723a739d00cff4b17f255d
VmNjord <alivenjord@gmail.com>
2025-02-12 18:08:03 +0300

    test 2.3.2.

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -7,22 +7,22 @@
 const score = 50 // тестовое значение, можно изменять
 let grade
 // your code
-if (score === 100 & score >= 90) {
+if (score === 100 && score >= 90) {
     grade = "A" 
 } else if
-(score <= 89 & score >= 80) {
+(score <= 89 && score >= 80) {
 grade = "B"
 }
 else if
-(score <= 79 & score >= 70) {
+(score <= 79 && score >= 70) {
 grade = "C"
 }
 else if
-(score <= 69 & score >= 50) {  
+(score <= 69 && score >= 50) {  
 grade = "D"
 }
 else if
-(score <= 49 & score >= 0) {
+(score <= 49 && score >= 0) {
 grade = "F"
 }
 console.log(grade); // Выводим оценку
